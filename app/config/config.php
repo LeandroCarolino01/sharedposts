84291cbafe64aae735b48e7eb05e1e3ec7580d0a
Leandro.Carolino <carolino.leandro@gmail.com>
2020-01-11 01:05:35 +0100

    initial commit

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+    //DB Params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '123456');
+    define('DB_NAME', 'sharedposts');
+    // App root
+    define('APPROOT',dirname(dirname( __FILE__)));
+    // URL root
+    define('URLROOT', 'http://localhost/sharedposts');
+    // Site name
+    define('SITENAME', 'Sharedposts');
