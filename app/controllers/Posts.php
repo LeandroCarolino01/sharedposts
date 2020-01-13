0ae4f4b6d91f77ffcb8d07d4801102ca311e2155
Leandro.Carolino <carolino.leandro@gmail.com>
2020-01-13 20:14:24 +0100

    edit view added

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -63,6 +63,61 @@
             
         }
 
+
+        public function edit($id){
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                // sanitize POST array
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+                $data = [
+                    'id' => $id,
+                    'title' => trim($_POST['title']),
+                    'body' => trim($_POST['body']),
+                    'user_id' => $_SESSION['user_id'],
+                    'title_err' => '',
+                    'body_err' => ''
+                ];
+
+                // validate title
+                if(empty($data['title'])){
+                    $data['title_err'] = 'Please enter title';
+                }
+
+                if(empty($data['body'])){
+                    $data['body_err'] = 'Please enter body text';
+                }
+
+                // make sure no errors
+                if(empty($data['title_err']) && empty($data['body_err'])){
+                    // validate
+                    if($this->postModel->updatePost($data)){
+                        flash('post_message', 'Post updatted');
+                        redirect('posts');
+                    } else {
+                        die('something went wrong');
+                    }
+                } else {
+                    $this->view('posts/edit', $data);
+                }
+
+            } else {
+                // get existing from model
+                $post = $this->postModel->getPostById($id);
+
+                // check for owner
+                if($post->user_id != $_SESSION['user_id']){
+                    redirect('posts');
+                }
+                $data = [
+                    'id' => $id,
+                    'title' => $post->title,
+                    'body' => $post->body
+                ];
+    
+                $this->view('posts/edit', $data);
+            }
+            
+        }
+
         public function show($id){
             $post = $this->postModel->getPostById($id);
             $user = $this->userModel->getUserById($post->user_id);
