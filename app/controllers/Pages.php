9359315378a547669b18106a6b4d6d9178b2259a
Leandro.Carolino <carolino.leandro@gmail.com>
2020-01-13 13:07:41 +0100

    post form added

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -5,6 +5,9 @@
         }
 
         public function index(){
+            if(isLoggedIn()){
+                redirect('posts');
+            }
             $data = [
                 'title' => 'SharedPosts', 
                 'description' => 'Simple social network built on Leandros PHP framework'
